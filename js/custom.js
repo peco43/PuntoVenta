05a93ff0261f8fe568b6a9d3c3897c940278f379
peco43 <peco43@gmail.com>
2017-04-06 11:00:18 -0600

    Adaptacion de Plantilla
    Cambio de Iconos ,Menus,Mejoras de tables a data tables

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+				$('#example').DataTable();
+			} );
+
+$('#example')
+		.removeClass( 'display' )
+		.addClass('table table-striped table-bordered');
+
+$(document).ready(function() {
+				$('#tabla1').DataTable();
+			} );
+
+$('#tabla1')
+		.removeClass( 'display' )
+		.addClass('table table-striped table-bordered');
+
+$(document).ready(function() {
+				$('#tabla2').DataTable();
+			} );
+
+$('#tabla2')
+		.removeClass( 'display' )
+		.addClass('table table-striped table-bordered');
